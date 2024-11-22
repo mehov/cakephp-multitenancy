2125203e3e86456741dd57ba5cee11315e391de0
mehov <mehov@users.noreply.github.com>
2024-11-22 16:26:12 +0000

    Add docBlocks to src/Account.php

--- src/Account.php
+++ src/Account.php
@@ -5,26 +5,41 @@ namespace Multitenancy;
 class Account
 {
 
+    /**
+     * @return string
+     */
     private static function getPlugin()
     {
         return __NAMESPACE__;
     }
 
+    /**
+     * @return \Cake\Http\Session
+     */
     public static function getSession()
     {
         return new \Cake\Http\Session();
     }
 
+    /**
+     * @return string
+     */
     private static function getSessionKey()
     {
         return sprintf('%s.%s', self::getPlugin(), 'Account');
     }
 
+    /**
+     * @param \Multitenancy\Model\Entity\Account $account
+     */
     public static function set($account)
     {
         $account = self::getSession()->write(self::getSessionKey(), $account);
     }
 
+    /**
+     * @return \Multitenancy\Model\Entity\Account
+     */
     public static function get()
     {
         return self::getSession()->read(self::getSessionKey());
