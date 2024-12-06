51f055806f807008a8522d8b77624ef851833160
mehov <mehov@users.noreply.github.com>
2024-12-06 13:56:58 +0000

    Remove unused Plugin::bootstrap()

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,9 +8,4 @@ use Cake\Core\PluginApplicationInterface;
 class Plugin extends BasePlugin
 {
 
-    public function bootstrap(PluginApplicationInterface $app): void
-    {
-        parent::bootstrap($app);
-    }
-
 }
